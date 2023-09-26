b089faa1e1f783446de9a237b3024e29622c917a
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-09-25 22:09:15 -0400

    Create essentials.php

---
+++ Front-End/partials/essentials.php
@@ -0,0 +1,92 @@
+<div class="pro-container">
+    <div class="pro" id="shrink-border">
+        <img src="img/products/pencil 1.jpg" alt="Kalour Professional Sketching Pencils 12pc">
+        <div class="des">
+            <span>kalour</span>
+            <h5>Professional Sketching Pencils</h5>
+            <div class="star">
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+            </div>
+            <h4>$7</h4>
+        </div>
+    <a href="#">
+        <i class="material-symbols-rounded cart">shopping_cart</i>
+        <i class="material-symbols-rounded add">add_circle</i>
+    </a>
+    </div>
+    <div class="pro" id="shrink-border">
+        <img src="img/products/pen 2.jpg" alt="ARTLINE Drawing System Set of 4 ( 0.1, 0.3, 05, 0.7 ) 4PSH1">
+        <div class="des">
+            <span>artline</span>
+            <h5>Drawing System Set</h5>
+            <div class="star">
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+            </div>
+            <h4>$11</h4>
+        </div>
+    <a href="#">
+        <i class="material-symbols-rounded cart">shopping_cart</i>
+        <i class="material-symbols-rounded add">add_circle</i>
+    </a>
+    </div>
+    <div class="pro" id="shrink-border">
+        <img src="img/products/acrylic 1a.jpg" alt="Acrylic Gouache Set Primaries">
+        <div class="des">
+            <span>liquitex</span>
+            <h5>Acrylic Gouache Set</h5>
+            <div class="star">
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+            </div>
+            <h4>$75</h4>
+        </div>
+    <a href="#">
+        <i class="material-symbols-rounded cart">shopping_cart</i>
+        <i class="material-symbols-rounded add">add_circle</i>
+    </a>
+    </div>
+    <div class="pro" id="shrink-border">
+        <img src="img/products/oil 1.jpg" alt="Blick Studio Oil Colors">
+        <div class="des">
+            <span>blick studio</span>
+            <h5>Oil Color Starter Set</h5>
+            <div class="star">
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+            </div>
+            <h4>$35</h4>
+        </div>
+    <a href="#">
+        <i class="material-symbols-rounded cart">shopping_cart</i>
+        <i class="material-symbols-rounded add">add_circle</i>
+    </a>
+    </div>
+    <div class="pro" id="shrink-border">
+        <img src="img/products/palette 1.jpg" alt="Blick Palette Paper">
+        <div class="des">
+            <span>blick studio</span>
+            <h5>Palette Paper</h5>
+            <div class="star">
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+                <i class="material-symbols-rounded">grade</i>
+            </div>
+            <h4>$23</h4>
+        </div>
+    <a href="#">
+        <i class="material-symbols-rounded cart">shopping_cart</i>
+        <i class="material-symbols-rounded add">add_circle</i>
+    </a>
+    </div>
+</div>
