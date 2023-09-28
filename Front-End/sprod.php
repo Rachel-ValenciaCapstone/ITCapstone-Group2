e3e010deb32fc97d82eeeef52e2f5fe4d53bb002
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-09-27 23:31:48 -0400

    Update sprod.php
    Added an anchor to go back to shop page, changed "Add to Cart" button

--- Front-End/sprod.php
+++ Front-End/sprod.php
@@ -6,7 +6,7 @@
     <style>  
         .fa {  
             margin: 0 4px;  
-            font-size: 22px;  
+            font-size: 18px;  
             color: #47384a;
         }  
     </style>
@@ -46,22 +46,20 @@
                 <div class="small-img-col">
                     <img src="img/products/pencil 4.jpg" width="100%" class="small-img" alt="Kalour Professional Sketching Pencils 12pc">
                 </div>
-
             </div>
         </div>
 
         <div class="single-pro-details">
-            <h6>Home / Materials</h6>
+            <a href="shop.php" class="fa fa-angle-left arrow"><small class="back">Shop</small></a>
             <h4>Kalour Professional Sketching Pencils 12pc</h4>
             <h2>$7.00</h2>
-            <input type="number" value="1">
-            <button class="button-48"><span class="text">Add To Cart</span></button>
+            <button class="button-28"><div class="text">ADD TO CART</div></button>
             <h4>Product Details</h4>
             <span>Premium sharpened graphite pencils for drawing (5H, 4H, 3H, 2H,HB, B, 2B, 3B, 4B, 5B, 6B, 8B).A wide variety of hardness for all your artwork needs.harder degrees for outlining (2B - 5H) and softer degrees for blending and layering (3B - 8B).Drawing Pencils enable creating lines of any thickness and color saturation.</span>
         </div>
     </section>
 
-    <!-- featured products -->
+    <!-- essential products -->
     <section id="product1" class="singleprod">
         <h2>SHOP OUR ESSENTIALS</h2>
         <p>Empowering Artists with the Finest Essentials!</p>
