258f1b8dd6d261b3c1a3d0ea3ccac6cf8c5bd0b2
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-10-06 18:57:17 -0400

    Create account.php

---
+++ Front-End/partials/account.php
@@ -0,0 +1,34 @@
+<div class="sub-menu">
+    <div class="user-info">
+        <h3>Hello, artistic spirit</h3>
+    </div>
+    <hr>
+    <a href="cart.php" class="sub-menu-link">
+        <i class="material-symbols-rounded">shopping_cart</i>
+        <p>View Cart</p>
+        <span>></span>
+    </a>
+    <a href="#" class="sub-menu-link">
+        <i class="material-symbols-rounded">favorite</i>
+        <p>Wishlist</p>
+        <span>></span>
+    </a>
+    <a href="#" class="sub-menu-link">
+        <i class="material-symbols-rounded">local_shipping</i>
+        <p>Track Order</p>
+        <span>></span>
+    </a>
+    <a href="#" class="sub-menu-link">
+        <i class="material-symbols-rounded">Help</i>
+        <p>help</p>
+        <span>></span>
+    </a>
+    <!-- <a href="#" class="sub-menu-link">
+        <i class="material-symbols-rounded">logout</i>
+        <p>Logout</p>
+        <span>></span>
+    </a> -->
+</div>
+
+<script src="account.js"></script>
+
