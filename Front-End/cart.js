160192f48758b76c32bf11fa707cd0b65ac9ba51
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-11-06 20:58:22 -0500

    Update cart.js

--- Front-End/cart.js
+++ Front-End/cart.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-// formats card number to #### #### #### ####
+// Formats card number to #### #### #### ####
 function formats(ele, e) {
     if (ele.value.length < 19) {
         ele.value = ele.value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
@@ -15,70 +15,85 @@ function numberValidation(e) {
     return false;
 }
 
-// all initial elements
-const payAmountBtn = document.querySelector('#payAmount');
-const decrementBtn = document.querySelectorAll('#decrement');
-const incrementBtn = document.querySelectorAll('#increment');
-const quantityElem = document.querySelectorAll('#quantity');
-const priceElem = document.querySelectorAll('#price');
-const subtotalElem = document.querySelector('#subtotal');
-const taxElem = document.querySelector('#tax');
-const totalElem = document.querySelector('#total');
+document.addEventListener('DOMContentLoaded', function () {
+    // All initial elements
+    const payAmountBtn = document.querySelector('#payAmount');
+    const decrementBtn = document.querySelectorAll('#decrement');
+    const incrementBtn = document.querySelectorAll('#increment');
+    const quantityElem = document.querySelectorAll('#quantity');
+    const priceElem = document.querySelectorAll('#price');
+    const subtotalElem = document.querySelector('#subtotal');
+    const taxElem = document.querySelector('#tax');
+    const totalElem = document.querySelector('#total');
+    const removeButtons = document.querySelectorAll('.product-close-btn');
 
-// loop for add event on multiple 'increment' and 'decrement' button
-for (let i = 0; i < incrementBtn.length; i++) {
-    incrementBtn[i].addEventListener('click', function () {
-        let increment = Number(this.previousElementSibling.textContent);
-        increment++;
+    // Function to calculate the total amount
+    function totalCalc() {
+        const tax = 0.065;
+        let subtotal = 0;
+        let totalTax = 0;
+        let shipping = 0;
+        let total = 0;
 
-        this.previousElementSibling.textContent = increment;
+        for (let i = 0; i < quantityElem.length; i++) {
+            subtotal += Number(quantityElem[i].textContent) * Number(priceElem[i].textContent);
+        }
 
-        totalCalc();
-    });
+        subtotalElem.textContent = subtotal.toFixed(2);
 
-    decrementBtn[i].addEventListener('click', function () {
-        let decrement = Number(this.nextElementSibling.textContent);
-        decrement <= 1 ? 1 : decrement--;
+        totalTax = subtotal * tax;
 
-        this.nextElementSibling.textContent = decrement;
+        taxElem.textContent = totalTax.toFixed(2);
 
-        totalCalc();
-    });
-}
+        total = subtotal + totalTax + shipping;
 
-const totalCalc = function () {
-
-    const tax = 0.065;
-    let subtotal = 0;
-    let totalTax = 0;
-    let shipping = 6.99;
-    let total = 0;
-
-    for (let i = 0; i < quantityElem.length; i++) {
-
-        subtotal += Number(quantityElem[i].textContent) * Number(priceElem[i].textContent);
-
+        totalElem.textContent = total.toFixed(2);
+        payAmountBtn.textContent = total.toFixed(2);
     }
 
-    subtotalElem.textContent = subtotal.toFixed(2);
+    // Add event listeners for increment and decrement buttons
+    for (let i = 0; i < incrementBtn.length; i++) {
+        incrementBtn[i].addEventListener('click', function () {
+            let increment = Number(this.previousElementSibling.textContent);
+            increment++;
 
-    totalTax = subtotal * tax;
+            this.previousElementSibling.textContent = increment;
 
-    taxElem.textContent = totalTax.toFixed(2);
+            totalCalc();
+        });
 
-    total = subtotal + totalTax + shipping;
+        decrementBtn[i].addEventListener('click', function () {
+            let decrement = Number(this.nextElementSibling.textContent);
+            decrement = decrement <= 1 ? 1 : decrement - 1;
 
-    totalElem.textContent = total.toFixed(2);
-    payAmountBtn.textContent = total.toFixed(2);
-}
+            this.nextElementSibling.textContent = decrement;
 
-// automatically formats US 10 digit phone number
+            totalCalc();
+        });
+    }
 
-function phoneFormat(input) {
-    input = input.replace(/\D/g, '');
-    var size = input.length;
-    if (size > 0) { input = input }
-    if (size > 2) { input = input.slice(0, 3) + "-" + input.slice(3, 11) }
-    if (size > 6) { input = input.slice(0, 7) + "-" + input.slice(7) }
-    return input;
-}
+    // Add event listeners to the "Remove" buttons
+    removeButtons.forEach((button, index) => {
+        button.addEventListener('click', function () {
+            const productCard = this.closest('.product-card');
+            if (productCard) {
+                productCard.remove();
+                // After removing the item, recalculate the total price.
+                if (quantityElem[index]) {
+                    quantityElem[index].textContent = 0; // Set quantity to 0 when removed
+                }
+                totalCalc();
+            }
+        });
+    });
+
+    // Automatically formats US 10 digit phone number
+    function phoneFormat(input) {
+        input = input.replace(/\D/g, '');
+        var size = input.length;
+        if (size > 0) { input = input }
+        if (size > 2) { input = input.slice(0, 3) + "-" + input.slice(3, 11) }
+        if (size > 6) { input = input.slice(0, 7) + "-" + input.slice(7) }
+        return input;
+    }
+});
