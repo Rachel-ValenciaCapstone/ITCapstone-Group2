f7196021c257fedc7fe1da2fb333d07b242b8ceb
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-11-06 21:12:49 -0500

    Update cart.js

--- Front-End/cart.js
+++ Front-End/cart.js
@@ -7,13 +7,13 @@ function formats(ele, e) {
         return true;
     } else {
         return false;
-    };
-};
+    }
+}
 
 function numberValidation(e) {
     e.target.value = e.target.value.replace(/[^\d ]/g, '');
     return false;
-};
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     // All initial elements
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         for (let i = 0; i < quantityElem.length; i++) {
             subtotal += Number(quantityElem[i].textContent) * Number(priceElem[i].textContent);
-        };
+        }
 
         subtotalElem.textContent = subtotal.toFixed(2);
 
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         totalElem.textContent = total.toFixed(2);
         payAmountBtn.textContent = total.toFixed(2);
-    };
+    }
 
     // Add event listeners for increment and decrement buttons
     for (let i = 0; i < incrementBtn.length; i++) {
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             totalCalc();
         });
-    };
+    }
 
     // Add event listeners to the "Remove" buttons
     removeButtons.forEach((button, index) => {
@@ -83,7 +83,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     quantityElem[index].textContent = 0; // Set quantity to 0 when removed
                 }
                 totalCalc();
-            };
+            }
         });
     });
 });
@@ -96,4 +96,4 @@ function phoneFormat(input) {
     if (size > 2) { input = input.slice(0, 3) + "-" + input.slice(3, 11) }
     if (size > 6) { input = input.slice(0, 7) + "-" + input.slice(7) }
     return input;
-};
+}
