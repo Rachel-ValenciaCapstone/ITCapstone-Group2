d8da89ff74c11a0b186fb2e9890592e9856283ef
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-11-25 01:06:19 -0500

    Update accountdash.php

--- Front-End/accountdash.php
+++ Front-End/accountdash.php
@@ -30,9 +30,9 @@
             <ul class="account-navbar">
                 <li><a href="#order-history" class="link">Orders</a></li>
                 <!-- <span class="nav-separator">|</span> -->
-                <li><a href="#profile-manager" class="link">Profile Manager</a></li>
+                <li><a href="#account-details" class="link">Account</a></li>
                 <!-- <span class="nav-separator">|</span> -->
-                <li><a href="#account-setting" class="link">Account Settings</a></li>
+                <li><a href="#more-settings" class="link">Settings</a></li>
             </ul>
         </nav>
 
@@ -52,7 +52,7 @@
                                 <div class="order-list-detail input-flex">
                                     <div class="order-detail">
                                         <!-- generate order number in invrements of 1 using id="order_id" -->
-                                        <a href="orderconf.php"><span class="detail-header">Order ID: <span id="order_num">00000001</span></span></a><br>
+                                        <a href="orderconf.php" target="_blank"><span class="detail-header">Order ID: <span id="order_num">00000001</span></span></a><br>
                                         <span class="sub-detail">Items ordered: <span id="num_items">2</span></span><br>
                                         <!-- ID same as cart, shipping & order confirmation page -->
                                         <span class="sub-detail">Total: $<span id="total">19.17</span></span><br>
@@ -71,7 +71,7 @@
                                 <div class="order-list-detail input-flex">
                                     <div class="order-detail">
                                         <!-- generate order number in invrements of 1 using id="order_id" -->
-                                        <a href="orderconf.php"><span class="detail-header">Order ID: <span id="order_num">00000002</span></span></a><br>
+                                        <a href="orderconf.php" target="_blank"><span class="detail-header">Order ID: <span id="order_num">00000002</span></span></a><br>
                                         <span class="sub-detail">Items ordered: <span id="num_items">1</span></span><br>
                                         <!-- ID same as cart, shipping & order confirmation page -->
                                         <span class="sub-detail">Total: $<span id="total">79.88</span></span><br>
@@ -82,25 +82,56 @@
                     </div>
                 </div>
 
-                <div class="dash-profile-manager">
-                    <h3 id="profile-manager">Profile Manager</h3>
-                    <div class="profile-dash">
-                        <div class="profile-header input-flex">
+                <div class="dash-account-details">
+                    <h3 id="account-details">Account Details</h3>
+                    <div class="account-dash">
+                        <div class="account-info">
                             <h4>User Info</h4>
-                            <div class="user-detail">
-                                <!-- retreive first and last name data from signup -->
-                                <div class="input-flex">
-                                    <div style="width: 60%;">
-                                        <label for="first-name" class="label-default">First name</label>
-                                        <input type="text" id="first-name" value="Palette" readonly>
-                                    </div>
-                                    <div style="width: 60%;">
-                                        <label for="last-name" class="label-default">Last name</label>
-                                        <input type="text" id="last-name" value="Paradise" readonly>
-                                    </div>
-                                </div>
+                            <div>
+                                <!-- get first name from signup -->
+                                <label for="first-name" class="label-default">First name</label>
+                                <input type="text" id="first-name" value="Palette" readonly>
+                            </div>
+                            <div>
+                                <!-- get last name from signup -->
+                                <label for="last-name" class="label-default">Last name</label>
+                                <input type="text" id="last-name" value="Paradise" readonly>
+                            </div>
+                            <div>
+                                <!-- get phone number from shipping -->
+                                <label for="phone" class="label-default">Phone Number</label>
+                                <input type="tel" id="phone" value="555-123-4567" readonly>
+                            </div>
+                            <div>
+                                <!-- get email from shipping -->
                                 <label for="email" class="label-default">Email</label>
                                 <input type="email" id="email" value="palette.paradise@gmail.com" readonly>
+                            </div>
+                            <h4>Payment Methods</h4>
+                            <p>You currently don&apos;t have any saved payment methods. You can add a method in the next update.</p>
+                            <h4>Delivery Addresses</h4>
+                            <p>You currently don&apos;t have any saved delivery addresses. You can add an address in the next update.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="dash-more-settings">
+                    <h3 id="more-settings">More Settings</h3>
+                    <div class="settings-dash">
+                        <div class="settings-options">
+                            <h4>Communication Preferences</h4>
+                            <p>Get updates on products, offers, and your member benefits.</p>
+                            <div class="method selected">
+                                <button class="method selected">
+                                    <ion-icon class="checkmark fill" name="checkbox"></ion-icon>
+                                    <span>No, don&apos;t send me emails.</span>
+                                </button>
+                            </div>
+                            <div class="method">
+                                <button class="method">
+                                    <ion-icon class="checkmark" name="checkbox-outline"></ion-icon>
+                                    <span>Yes, send me emails.</span>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -115,6 +146,9 @@
             <cite>Web icon by <a href="https://www.flaticon.com/authors/darius-dan">Flaticon</a></cite>
         </div>
     </footer>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 </body>
 
