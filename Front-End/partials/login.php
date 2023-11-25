3bde009b211add760a2e3f097548dff935c20952
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-11-25 01:11:30 -0500

    Update login.php

--- Front-End/partials/login.php
+++ Front-End/partials/login.php
@@ -1,7 +1,7 @@
 <div class="form_container">
     <i class="material-symbols-rounded form_close">close</i>
     <div class="form login_form">
-        <form action="#">
+        <form action="login.php">
             <h2>Login</h2>
             <div class="input_box">
                 <input type="email" placeholder="Enter your email" required /><i class="material-symbols-rounded email">mail</i>
@@ -25,20 +25,28 @@
     </div>
 
     <div class="form signup_form">
-        <form action = "registration_process.php" method="post">
+        <form action="signup-process.php" method="post">
             <h2>Signup</h2>
-            <div class="input_box">
-                <input type="email" name="email" placeholder="Enter your email" required /><i class="material-symbols-rounded email">mail</i>
+            <div class="input-flex">
+                <div class="input_box">
+                    <input type="text" id="first-name" placeholder="First name" required /><i class="material-symbols-rounded email">assignment_ind</i>
+                </div>
+                <div class="input_box">
+                    <input type="text" id="last-name" placeholder="Last name" required /><i class="material-symbols-rounded email">assignment_ind</i>
+                </div>
             </div>
             <div class="input_box">
-                <input type="password" name="password" placeholder="Create password" required /><i class="material-symbols-rounded password">lock</i>
+                <input type="email" placeholder="Enter your email" required /><i class="material-symbols-rounded email">mail</i>
+            </div>
+            <div class="input_box">
+                <input type="password" placeholder="Create password" required /><i class="material-symbols-rounded password">lock</i>
                 <i class="fa fa-eye-slash pw_hide"></i>
             </div>
             <div class="input_box">
-                <input type="password" name="confirm_password" placeholder="Confirm password" required /><i class="material-symbols-rounded password">lock</i>
+                <input type="password" placeholder="Confirm password" required /><i class="material-symbols-rounded password">lock</i>
                 <i class="fa fa-eye-slash pw_hide"></i>
             </div>
-            <button class="button" name="submit">Signup Now</button>
+            <button class="button">Signup Now</button>
             <div class="login_signup">
                 Already have an account? <a href="#" id="login">Login</a>
             </div>
