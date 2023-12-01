eab5e2563a07a3f0566129a0d04b9b3248d4e172
BambiLee1 <144631259+BambiLee1@users.noreply.github.com>
2023-12-01 05:52:13 -0500

    Update login.php
    Added Name boxes for the email and password input boxes. Updated the links to registration and login PHP pages.

--- Front-End/partials/login.php
+++ Front-End/partials/login.php
@@ -1,13 +1,13 @@
 <div class="form_container">
     <i class="material-symbols-rounded form_close">close</i>
     <div class="form login_form">
-        <form action="login.php">
+        <form action="login_process.php" method="post">
             <h2>Login</h2>
             <div class="input_box">
-                <input type="email" placeholder="Enter your email" required /><i class="material-symbols-rounded email">mail</i>
+                <input type="email" name="email" placeholder="Enter your email" required /><i class="material-symbols-rounded email">mail</i>
             </div>
             <div class="input_box">
-                <input type="password" placeholder="Enter your password" required /><i class="material-symbols-rounded password">lock</i>
+                <input type="password" name="password" placeholder="Enter your password" required /><i class="material-symbols-rounded password">lock</i>
                 <i class="fa fa-eye-slash pw_hide"></i>
             </div>
             <div class="option_field">
@@ -17,7 +17,7 @@
                 </span>
                 <a href="#" class="forgot_pw">Forgot password?</a>
             </div>
-            <button class="button">Login Now</button>
+            <button class="button" name="submit">Login Now</button>
             <div class="login_signup">
                 Don't have an account? <a href="#" id="signup">Signup</a>
             </div>
@@ -25,28 +25,20 @@
     </div>
 
     <div class="form signup_form">
-        <form action="signup-process.php" method="post">
+        <form action = "registration_process.php" method="post">
             <h2>Signup</h2>
-            <div class="input-flex">
-                <div class="input_box">
-                    <input type="text" id="first-name" placeholder="First name" required /><i class="material-symbols-rounded email">assignment_ind</i>
-                </div>
-                <div class="input_box">
-                    <input type="text" id="last-name" placeholder="Last name" required /><i class="material-symbols-rounded email">assignment_ind</i>
-                </div>
+            <div class="input_box">
+                <input type="email" name="email" placeholder="Enter your email" required /><i class="material-symbols-rounded email">mail</i>
             </div>
             <div class="input_box">
-                <input type="email" placeholder="Enter your email" required /><i class="material-symbols-rounded email">mail</i>
-            </div>
-            <div class="input_box">
-                <input type="password" placeholder="Create password" required /><i class="material-symbols-rounded password">lock</i>
+                <input type="password" name="password" placeholder="Create password" required /><i class="material-symbols-rounded password">lock</i>
                 <i class="fa fa-eye-slash pw_hide"></i>
             </div>
             <div class="input_box">
-                <input type="password" placeholder="Confirm password" required /><i class="material-symbols-rounded password">lock</i>
+                <input type="password" name="confirm_password" placeholder="Confirm password" required /><i class="material-symbols-rounded password">lock</i>
                 <i class="fa fa-eye-slash pw_hide"></i>
             </div>
-            <button class="button">Signup Now</button>
+            <button class="button" name="submit">Signup Now</button>
             <div class="login_signup">
                 Already have an account? <a href="#" id="login">Login</a>
             </div>
@@ -55,3 +47,4 @@
 </div>
 
 <script src="login.js"></script>
+
